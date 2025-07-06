cc7a79074e748285c72fd3ef878cca7d39d351af
SOHYAMAZ <snow.sohei@gmail.com>
2025-07-06 12:03:04 +0900

    refactor(exit):add exit func for syscall

--- srcs/exit.c
+++ srcs/exit.c
@@ -6,7 +6,7 @@
 /*   By: sohyamaz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 20:50:14 by sohyamaz          #+#    #+#             */
-/*   Updated: 2025/06/19 22:12:22 by sohyamaz         ###   ########.fr       */
+/*   Updated: 2025/07/06 12:01:46 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,24 @@ void	closing_fds(t_structs *var)
 	return ;
 }
 
+void	put_perror(int error)
+{
+	if (error < 7)
+		return ;
+	else if (error == 7)
+		perror("fork");
+	else if (error == 8)
+		perror("open");
+	else if (error == 9)
+		perror("pipe");
+	else if (error == 10)
+		perror("dup2");
+	else if (error == 11)
+		perror("access");
+	else if (error == 12)
+		perror("execve");
+	return ;
+}
 
 void	free_all(t_structs *var)
 {
@@ -49,6 +67,8 @@ void	error_exit(t_structs **var, int error)
 {
 	if (error < 7)
 		ft_putnbr_fd(error, 2);
+	else
+		put_perror(error);
 	free_all(*var);
 	exit (error);
 }
