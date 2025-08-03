6b83390584664ada0a1f28d08042509cd674e549
zaki2ubw <snow.sohei@gmail.com>
2025-08-03 20:09:51 +0900

    fix(norm):format this file based on norminette

--- PIPEX/srcs/main.c
+++ PIPEX/srcs/main.c
@@ -1,13 +1,13 @@
 #include "pipex.h"
 
-int		main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
 	t_parent	*master;
 	int			exitcode;
 
 	exitcode = 0;
 	master = NULL;
-	//check_argc(argc);
+	check_argc(argc);
 	init_parent(argc, &master);
 	make_list(master);
 	place_args(argc, argv, master);
